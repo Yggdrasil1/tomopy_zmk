75ae00c0582e197b4529f401d9048ae8a85fb08b
Recon2 - Kai <recon2@c09vm-jwi-f02v2.charite.de>
2018-05-24 18:15:00 +0200

    läuft!

--- recon_edf_tomopy_imprvd.py
+++ recon_edf_tomopy_imprvd.py
@@ -42,19 +42,34 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     fname = args.filepath
+  
+    
     StartSlice = args.StartSlice
     number_recon_slices = args.number_recon_slices
     rot_center = args.rotation_center
     alg = args.algorithm
 
-    dirpath = os.path.dirname(fname)
-    number_of_files = len(fnmatch.filter(os.listdir(dirpath), '*.edf'))
-    filename = fnmatch.filter(os.listdir(dirpath), '*.edf')[0]
+    #dirpath = os.path.dirname(fname)
+    
+    print("so your files are here?: ")
+    print(fname + "/n")
+    
+    print("you start at slice: " + str(StartSlice))
+    print("and you want: "+ str(number_recon_slices)+ " Slices")
+    
+    number_of_files = len(fnmatch.filter(os.listdir(fname), '*.edf'))
+    filename = fnmatch.filter(os.listdir(fname), '*.edf')[0]
+    
+    savename = fname + "/rec_" +  filename[:-4]
+    savedirectory = os.path.dirname(savename)
+    if not os.path.exists(savedirectory):
+        os.makedirs(savedirectory)
 
     List_names = []
 
     # chose alg from 'fbp', 'gridrec'....
     print("Preparing: ")
+    
     for i in range(number_of_files):
 
         loopnames = ""
@@ -75,14 +90,19 @@ if __name__ == '__main__':
         assert loopnames != "", "no files are given to the edf.reader"
 
     measure_file = dxchange.reader.read_edf(List_names[1], slc=None)
+    
+    if number_recon_slices==0:
+        number_recon_slices=len(measure_file[0])-StartSlice
 
     proj_ar = np.zeros((number_of_files, number_recon_slices, measure_file.shape[2]))
 
     for i in range( len(List_names)):
 
-        loopfile=dxchange.reader.read_edf(name, slc=None)
+        loopfile=dxchange.reader.read_edf(List_names[i], slc=None)
 
-        proj_ar[i]=loopfile[StartSlice:(number_recon_slices + StartSlice), :]
+        proj_ar[i]=loopfile[0,StartSlice:(number_recon_slices + StartSlice), :]
+        
+        print('\r'+"progress: " + str(i) + "/ " + str(number_of_files), end = '')
 
     theta = tomopy.angles(number_of_files)
 
@@ -91,3 +111,17 @@ if __name__ == '__main__':
         assert rot_center>0, "rot_center is probably wrong"
 
     proj = tomopy.minus_log(proj_ar)
+    print("")
+    print("begin reconstruction using ["+alg+ "] approach...")  
+    rec = tomopy.recon(proj, theta, center=rot_center, algorithm=alg)
+    
+    # Mask each reconstructed slice with a circle.
+    rec1 = tomopy.circ_mask(rec, axis=0, ratio=0.95)
+    #plt.show(block=False)
+    #plt.imshow(rec1[0])
+    #plt.show
+    
+    # Write data as stack of TIFs.
+    dxchange.write_tiff_stack(rec1, savename+'_recon_')    
+    
+    print("everything finished, find the results here: ",savename)
