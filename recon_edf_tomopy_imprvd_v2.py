2494a78de93570b9087e6933a05cc3abdde2f3db
Yggdrasil1 <k.a.i.winkler@gmx.de>
2018-06-06 15:51:27 +0200

    resizing added, outputfolder and timestamp

--- recon_edf_tomopy_imprvd_v2.py
+++ recon_edf_tomopy_imprvd_v2.py
@@ -7,11 +7,14 @@ import numpy as np
 import fnmatch
 import os
 from matplotlib import pyplot as plt
-from PIL import Image
-
+from scipy.misc import imresize
+import time
+import datetime
 import math
 
 if __name__ == '__main__':
+    
+    time_start = time.time()
   
     fname = str(raw_input('Please paste the absolut path to the edf-files here: '))
     assert fname != "", "Your filepath cannot be empty!"
@@ -19,14 +22,28 @@ if __name__ == '__main__':
     StartSlice = int(input('At which Slice do you wish to start' + 
                        ' the reconstruction?' + '\n The first one is 0: '))
     
-    number_recon_slices = int(input('How many slices do you want to reconstruct '
+    number_recon_slices = int(input('How many slices do you want to reconstruct ' +
                                 'after the first one?: '))
     
-    rot_center = int(input('Where is the center of rotation? \nType 0 if you dont know: '))
+    rot_center = int(input('Where is the center of rotation? ' +
+                           '\nType 0 if you dont know: '))
     
-    alg = str(raw_input('Which algorithm do you wish to use? \nSome choises are "gridrec" "fbp": '))
+    alg = str(raw_input('Which algorithm do you wish to use? ' +
+                        '\nSome choises are "gridrec" "fbp": '))
     
-    print("so your files are here?: ")
+    factor_question = str(raw_input('Do you wish to resize the image ' +
+                                    '(to handle big datasets)? (y/n): '))
+    
+    if factor_question in ['y','Y','Yes','yes']:
+        
+        resize_parameter = input('Please type either the new image dimensions '
+                                  +'[eg. (1000x800)] \n'
+                                  +'or the % of the original size [eg. 50 = 50%]: ')
+        
+    output_folder = str(raw_input('Where do you want to safe the resulting images'
+                                  +' (a new folder gets created)?: '))
+    
+    print("\n so your files are here?: ")
     print(fname)
     
     print("you start at slice: " + str(StartSlice))
@@ -39,7 +56,7 @@ if __name__ == '__main__':
     
     print("---" + filename + "---")
     
-    savename = fname + "/rec_" +  filename[:-4]
+    savename = output_folder + "/rec_" + alg + "/" + os.path.splitext(os.path.basename(filename[:-4]))[0]
     
     savedirectory = os.path.dirname(savename)
     
@@ -70,8 +87,8 @@ if __name__ == '__main__':
         List_names.append(loopnames)
         assert loopnames != "", "no files are given to the edf.reader"
 
-   #measure_file = dxchange.reader.read_edf(List_names[1], slc=None)
     
+    #getting the image dimensions
     measure_file = dxchange.reader.read_edf(List_names[0], slc=None)
     height = measure_file.shape[1]
     width = measure_file.shape[2]
@@ -79,22 +96,25 @@ if __name__ == '__main__':
     measure_file = measure_file.reshape(height,width)
     
     if number_recon_slices==0:
-        number_recon_slices=len(measure_file[0])-StartSlice
+        number_recon_slices = len(measure_file[0])-StartSlice
 
-    proj_ar = np.zeros((number_of_files, number_recon_slices, measure_file.shape[2]))
+    #proj_ar = np.zeros((number_of_files, number_recon_slices, measure_file.shape[1]))
+    proj_ar=[]
 
-    for i in range( len(List_names)):
-
-        #loopfile=dxchange.reader.read_edf(List_names[i], slc=None)
+    for i in range(len(List_names)):
         
-        loopfile = dxchange.reader.read_edf(List_names[1], slc=None)
+        loopfile = dxchange.reader.read_edf(List_names[i], slc=None)
         
-        resl_loopfile = loopfile.resize(nwidth, nheight, Image.LANCZOS)
+        resl_loopfile = imresize(
+                loopfile[0,StartSlice:(number_recon_slices + StartSlice), :],
+                resize_parameter,
+                mode='F')
 
-        proj_ar[i]=loopfile[0,StartSlice:(number_recon_slices + StartSlice), :]
+        proj_ar.append(resl_loopfile)
         
-        print('\r'+"progress: " + str(i) + "/ " + str(number_of_files), end = '')
+        print('\r' + "progress: " + str(i+1) + "/ " + str(number_of_files),end='')
 
+    proj_ar = np.asarray(proj_ar)
     theta = tomopy.angles(number_of_files)
 
     if rot_center == 0:
@@ -115,4 +135,7 @@ if __name__ == '__main__':
     # Write data as stack of TIFs.
     dxchange.write_tiff_stack(rec1, savename+'_recon_')    
     
+    duration = str(datetime.timedelta(time.time()-time_start))
+    
+    print("It took %s (hh:mm:ss) to finish reconstruction" %(duration))
     print("everything finished, find the results here: ",savename)
